b7c727a6ce55123423eec8a4040277b64338de5b
Masi-s-Queleb <qnlebrun@hotmail.com>
2021-10-25 08:56:56 +0200

    add prototypes of socket and pygame example + dictionnary of name

---
+++ Prototypes/Agar-IO-master/Agar-IO-master/runClient.bat
@@ -0,0 +1,7 @@
+boomCraftEnvi1\Scripts\activate
+cd boomcraft
+cd Prototypes
+cd Agar-IO-master
+cd Agar-IO-master
+python game.py
+random
---
+++ Prototypes/Agar-IO-master/Agar-IO-master/runServer.bat
@@ -0,0 +1,6 @@
+boomCraftEnvi1\Scripts\activate
+cd boomcraft
+cd Prototypes
+cd Agar-IO-master
+cd Agar-IO-master
+python server.py

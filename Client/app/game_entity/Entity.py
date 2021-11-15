d22d93968f4ce5ead6c761c10d16f0ad39e5b2ad
Kevin-Neefs <64009245+Kevin-Neefs@users.noreply.github.com>
2021-11-15 13:26:45 +0100

    update entity class

--- Client/app/game_entity/Entity.py
+++ Client/app/game_entity/Entity.py
@@ -28,6 +28,34 @@ class Entity :
         print("look_in_game " + self.look_in_game)
 
 
+    """
+     return the area of tile occupied by the entity
+    """
+    def give_area(self):
+        area = self.coords[0] * self.coods[1]
+        return area
+
+    """
+     return the width of the entity in a number of tile
+    """
+    def give_width(self):
+        width = self.coords[0]
+        return width
+
+    """
+     return the height of the entity in a number of tile
+    """
+    def give_height(self):
+        height = self.coords[1]
+        return height
+
+    """
+     give the amount of ressource in parameter to the ressource counter
+    """
+    def give_ressources(ressource, amount):
+        dict_of_ressources[ressource] =+ amount  
+        print(amount + " of " + ressource + " dropped")
+
     
     def set_active(self,active):
             self.active = active

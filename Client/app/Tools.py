39dd8e41ca008fc05cc24d67e24efff0e1219720
Masi-s-Queleb <qnlebrun@hotmail.com>
2021-11-17 23:49:16 +0100

    add tools class

---
+++ Client/app/Tools.py
@@ -0,0 +1,9 @@
+import random 
+
+
+class Tools():
+    ''' 
+    Reunite all the usefull method
+    '''
+    def give_random_int_between(self, a: int, b: int) -> int :
+        return random.randint(a,b)

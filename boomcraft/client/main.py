09363c06251fe4194c299d3a3f74e42ae133691d
JohnLest <j_schwickerath@hotmail.com>
2022-01-01 12:40:09 +0100

    restructur and clear all project

---
+++ boomcraft/client/main.py
@@ -0,0 +1,6 @@
+def main():
+    print(f"Hello client")
+
+
+if __name__ == "__main__":
+    main()
---
+++ boomcraft/serveur/main.py
@@ -0,0 +1,6 @@
+def main():
+    print(f"Hello server")
+
+
+if __name__ == "__main__":
+    main()

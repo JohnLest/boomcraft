0a7be475613dbf0bf6fe99022cfdfd987c54e8f2
Masi-s-Queleb <qnlebrun@hotmail.com>
2022-01-12 20:06:32 +0100

    update Entity and Mobile

--- server/app/Tools.py
+++ server/app/Tools.py
@@ -5,7 +5,10 @@ class Tools():
     ''' 
     Reunite all the usefull method
     '''
-    def give_random_int_between(self, a: int, b: int) -> int :
+    def give_random_int_between(self, a: int, b: int) :
+        ''' 
+        :return a int
+        '''
         return random.randint(a,b)
 
 

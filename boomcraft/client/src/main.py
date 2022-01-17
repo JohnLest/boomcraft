99f4b1524baa99f1756138eeb10b60d94324eca7
John <j_schwickerath@hotmail.com>
2022-01-17 15:25:11 +0100

    add facebook / google / twitter authentication with flsak

--- boomcraft/client/src/main.py
+++ boomcraft/client/src/main.py
@@ -1,11 +1,18 @@
 from windows.mainWindow import MainWindow
 from connection import Connection
+import webbrowser
 
+
+# region main
 
 def main():
     print(f"Hello Client")
+    url = "http://localhost:8000"
     new_connection = Connection("127.0.0.1", 8080)
+    webbrowser.open(url)
     MainWindow(new_connection)
+
+# endregion
 
 
 if __name__ == "__main__":

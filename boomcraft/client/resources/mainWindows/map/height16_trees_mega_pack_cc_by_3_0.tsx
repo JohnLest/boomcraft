40954406b3dbeb9c5681eecfc3ea29f4dc61c33c
JohnLest <j_schwickerath@hotmail.com>
2022-01-23 00:07:21 +0100

    import some files

--- boomcraft/client/resources/mainWindows/map/height16_trees_mega_pack_cc_by_3_0.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="height16_trees_mega_pack_cc_by_3_0" tilewidth="16" tileheight="16" tilecount="361" columns="19">
- <image source="height16_trees_mega_pack_cc_by_3_0.png" width="314" height="304"/>
-</tileset>
